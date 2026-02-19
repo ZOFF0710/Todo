93385b5b8c3a8a12c16b36e41211b2745d9ad1bd
Fuku Syuu <luckyzoff01@gmail.com>
2026-02-19 22:26:21 +0900

    Add tab navigation and theme context with async storage support

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,44 @@
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+
+const TabsLayout = () => {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "red",
+        tabBarInactiveTintColor: "green",
+        tabBarStyle: {
+          backgroundColor: "blue",
+          borderTopWidth: 1,
+          borderTopColor: "green",
+        },
+        tabBarLabelStyle: {
+          fontSize: 12,
+          fontWeight: "600",
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Todos",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="flash-outline" size={size} color={color} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: "Settings",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="flash-outline" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+};
+
+export default TabsLayout;
